2dc64e0d2deb2cc86a38a19c064b80a7502b99f6
cheolgyu <rb7480@knou.ac.kr>
2021-12-31 00:01:04 +0900

    52주 작업중

--- model/tb_52_weeks.go
+++ model/tb_52_weeks.go
@@ -5,8 +5,13 @@ type Tb52Weeks struct {
 	Code_id int
 
 	Price_type int
+	Row_type   bool
 	Unit_type  int
 	Unit       int
-	High_price float32
-	Low_price  float32
+
+	Np_dt     int
+	Np_val    float32
+	Op_dt     int
+	Op_val    float32
+	P_percent float32
 }
